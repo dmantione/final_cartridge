f5d165987b9ec00a8453f56399f74367c681e2b7
Daniel Mantione <daniel.mantione@freepascal.org>
2026-07-25 17:20:01 +0200

    * Redo the layout of BASIC instructions with proper use of styles.

--- manual/rmtags.sh
+++ manual/rmtags.sh
@@ -1,15 +1,21 @@
+#!/bin/bash
+
+set -eu
+
 sed -z \
-    -E -e 's/style:font-name-complex=".*" ?//' \
-    -E -e 's/style:font-size-complex=".*" ?//' \
-    -E -e 's/style:font-weight-complex=".*" ?//' \
-    -E -e 's/style:language-complex=".*" ?//' \
-    -E -e 's/style:country-complex=".*" ?//' \
-    -E -e 's/style:language-comples=".*" ?//' \
-    -E -e 's/style:font-name-asian=".*" ?//' \
-    -E -e 's/style:font-size-asian=".*" ?//' \
-    -E -e 's/style:font-weight-asian=".*" ?//' \
-    -E -e 's/style:language-asian=".*" ?//' \
-    -E -e 's/style:country-asian=".*" ?//' \
-    -E -e 's/style:language-asian=".*" ?//' \
+    -E -e 's/style:font-name-complex="[^"]*" ?//g' \
+    -E -e 's/style:font-size-complex="[^"]*" ?//g' \
+    -E -e 's/style:font-weight-complex="[^"]*" ?//g' \
+    -E -e 's/style:language-complex="[^"]*" ?//g' \
+    -E -e 's/style:country-complex="[^"]*" ?//g' \
+    -E -e 's/style:font-name-asian="[^"]*" ?//g' \
+    -E -e 's/style:font-size-asian="[^"]*" ?//g' \
+    -E -e 's/style:font-weight-asian="[^"]*" ?//g' \
+    -E -e 's/style:language-asian="[^"]*" ?//g' \
+    -E -e 's/style:country-asian="[^"]*" ?//g' \
+    -E -e 's|<text:soft-page-break/>||g' \
+    -E -e 's|<config:config-item config:name="Rsid" config:type="int">[^<]*</config:config-item>||g' \
+    -E -e 's|<config:config-item config:name="RsidRoot" config:type="int">[^<]*</config:config-item>||g' \
     -E -e 's|<office:scripts>.*</office:scripts>||' \
-    $1
+    "${1:-/dev/stdin}"
+
