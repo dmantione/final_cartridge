01f3982adf32c1a356c21dba20b85959835c2be4
Daniel Mantione <daniel.mantione@freepascal.org>
2026-08-08 23:23:52 +0200

    * Add detection for some empty tags.

--- manual/rmtags.sh
+++ manual/rmtags.sh
@@ -14,11 +14,14 @@ sed -z \
     -E -e 's/style:language-asian="[^"]*" ?//g' \
     -E -e 's/style:country-asian="[^"]*" ?//g' \
     -E -e 's|<text:soft-page-break/>||g' \
+    -E -e 's|text:use-soft-page-breaks="true"|text:use-soft-page-breaks="false"|g' \
     -E -e 's|<config:config-item config:name="Rsid" config:type="int">[^<]*</config:config-item>||g' \
     -E -e 's|<config:config-item config:name="RsidRoot" config:type="int">[^<]*</config:config-item>||g' \
     -E -e 's|<config:config-item config:name="PrinterName" config:type="string">[^<]*</config:config-item>||g' \
+    -E -e 's|<config:config-item config:name="PrinterName" config:type="string"/>||g' \
     -E -e 's|<config:config-item config:name="PrinterSetup" config:type="base64Binary">[^<]*</config:config-item>||g' \
     -E -e 's|<config:config-item-map-indexed config:name="Views">.*</config:config-item-map-indexed>||g' \
+    -E -e 's|<config:config-item config:name="PrinterSetup" config:type="base64Binary"/>||g' \
     -E -e 's|<meta:document-statistic[^>]*/>||g' \
     -E -e 's|<meta:editing-duration>.*</meta:editing-duration>||g' \
     -E -e 's|<meta:editing-cycles>.*</meta:editing-cycles>||g' \
